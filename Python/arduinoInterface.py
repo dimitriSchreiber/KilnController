7f8d06db08449b1c90c7951c3691144c8430b35a
Dimitri <dschreib@eng.ucsd.edu>
2019-12-01 11:13:27 -0800

    temperature control + interface using timers

--- Python/arduinoInterface.py
+++ Python/arduinoInterface.py
@@ -1,80 +1,187 @@
-import serial
+#General use
 import time
-import re
+from datetime import datetime
+from datetime import timedelta
+
 import numpy as np
 
+#Microcontroller interfacing
+import serial
+import re
+
+#Data reading and logging
 import json
 import csv
 
+#Scheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+
+#--------------------------------------
+#Interfacing class
+#--------------------------------------
 class arduinoInterface():
-    def __init__(self, fig, **kw):
+    def __init__(self, dt = 5.0):
+        #User defined parameters
+        self.dt = dt
+
+        #Kiln state variables
         self.internalTemperature = 0.0
         self.kilnTemperature = 0.0
         self.kilnOn = False
         self.kilnHeartbeat = False
-
         self.startTime = time.time()
 
-        self.temperatureSetpoints = []
-        self.timeSetpoints = []
+        #AP scheduler for PWM generation
+        self.scheduler = BackgroundScheduler()
+        self.scheduler.start()
 
-        self.temperatureSetpointsInterpolated = []
-        self.timeSetpointsInterpolated = []
-        self.temperaturesMeasuredKiln = []
-        self.temperaturesMeasuredInternal = []
-        self.timesExecuted = []
+    #def __del__(self):
+    #    self.disconectSerial()
 
     def connectSerial(self, serialDevice):
         self.ser = serial.Serial(serialDevice, 115200) 
+        self.turnKilnOff()
 
     def disconectSerial(self):
+        self.turnKilnOff()
+        self.scheduler.shutdown(wait=True)
         self.ser.close()
 
-    def readTemperatureProfile(self, fileDirectory):
-        pass
+    def turnKilnOn(self):
+        self.ser.write(str(1).encode())
+        self.readLineFromArduino()
 
-    def turnKilnOn(self, on):
-        if on:
-            self.ser.write(str(1).encode())
-        else:
-            self.ser.write(str(0).encode())
-        timesExecuted.append(time.time()-self.startTime)
+    def turnKilnOff(self):
+        self.ser.write(str(0).encode())
+        self.readLineFromArduino()
 
     def readLineFromArduino(self):
         readLine = str(self.ser.readline())
         splitList = re.split(' ', readLine)
 
-        self.internalTemperature = float(splitList[2])
         self.kilnTemperature = float(splitList[1])
+        self.internalTemperature = float(splitList[2])
         self.kilnOn = int(splitList[3])
         self.kilnHeartbeat = int(splitList[5])
 
-        self.temperaturesMeasuredKiln.append(self.kilnTemperature)
-        self.temperaturesMeasuredInternal.append(self.internalTemperature)
+    def setKilnDutyCycle(self, dutyCycle):
+        if dutyCycle > 0.0:
+            onTime = dutyCycle * self.dt
+        else:
+            onTime = 0.0
+
+        offTime = self.dt - onTime
+        offTimeTime = datetime.now() + timedelta(seconds = onTime)
+
+        if onTime > 0.0:
+            self.scheduler.add_job(self.turnKilnOn, 'date', run_date = datetime.now())
+
+        if offTime > 0.0:
+            self.scheduler.add_job(self.turnKilnOff, 'date', run_date = offTimeTime)
+
+#--------------------------------------
+#Temperature controller class
+#--------------------------------------
+class TemperatureController():
+    def __init__(self, dt, interfaceObject, heatingElementPower = 1500):
+        #User defined parameters
+        self.dt = dt
+        self.interfaceObject = interfaceObject
+
+        #Enable temperature controller
+        self.enabled = False
+
+        #Setpoints and measured temperatures at each control cycle
+        self.setpoint = 0.0
+        self.temperatureSetpoints = []
+        self.temperaturesMeasuredKiln = []
+        self.temperaturesMeasuredInternal = []
+        self.timesExecuted = []
+
+        #Start time for time keeping subtraction
+        self.startTime = time.time()
+
+        #AP scheduler for controller execution
+        self.scheduler = BackgroundScheduler()
+        self.scheduler.start(paused=True)
+        self.scheduler.add_job(self.temperatureControl, 'interval', seconds = self.dt)
+
+    #def __del__(self):
+    #    self.disableController()
+    #    self.scheduler.shutdown()
+
+    def enableController(self):
+        self.scheduler.resume()
+
+    def shutdownController(self):
+        self.scheduler.pause()
+        self.scheduler.shutdown(wait=True)
+
+    def updateSetpoint(self, setpoint):
+        self.setpoint = setpoint
 
     def dumpData(self, dumpFile):
         with open(dumpFile, mode='w') as logging_file:
             logging_writer = csv.writer(logging_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            for kilnTemp, internalTemp, timeElement in zip(self.temperaturesMeasuredKiln, self.temperaturesMeasuredInternal, self.timesExecuted):
-                logging_writer.writerow([kilnTemp,internalTemp,timeElement])
+            for setpoint, kilnTemp, internalTemp, timeElement in zip(self.temperatureSetpoints, self.temperaturesMeasuredKiln, self.temperaturesMeasuredInternal, self.timesExecuted):
+                logging_writer.writerow([setpoint, kilnTemp,internalTemp,timeElement])
+
+    def calculateControlInput(self, setpoint):
+        dutyCycle = 1.0 * (setpoint - self.interfaceObject.kilnTemperature)
+        return np.clip(dutyCycle, 0.0, 1.0)
+
+    def temperatureControl(self):
+        dutyCycle = self.calculateControlInput(self.setpoint)
+        self.interfaceObject.setKilnDutyCycle(dutyCycle)
+
+        self.temperatureSetpoints.append(self.setpoint)
+        self.temperaturesMeasuredKiln.append(self.interfaceObject.kilnTemperature)
+        self.temperaturesMeasuredInternal.append(self.interfaceObject.internalTemperature)
+        self.timesExecuted.append(time.time()-self.startTime)
+
+        print('Time: {}, Setpoint Temperature: {}, Kiln Temperature: {}, Dutycycle: {}'.format(
+            time.time() - self.startTime, self.setpoint, self.interfaceObject.kilnTemperature, dutyCycle))
+
+#--------------------------------------
+#Temperature profile class
+#--------------------------------------
+class TemperatureProfile():
+    def __init__(self, controllerObject, temperatureFile):
+        #Read in temperature profile
+        self.temperatureSetpoints = []
+        self.timeSetpoints = []
+        self.readTemperatureProfile()
+
+        #Interpolated temperature profile based on dt
+        self.temperatureSetpointsInterpolated = []
+        self.timeSetpointsInterpolated = []
+
+
+    def readTemperatureProfile(self):
+        with open('data.txt') as json_file:
+            data = json.load(json_file)
+            for p in data['people']:
+                print('Name: ' + p['name'])
+                print('Website: ' + p['website'])
+                print('From: ' + p['from'])
+                print('')
+
 
 if __name__ == '__main__': 
 
-    interface = arduinoInterface()
+    dt = 1.0
+
+    interface = arduinoInterface(dt = dt)
     interface.connectSerial("/dev/ttyACM0")
+    controller = TemperatureController(dt = dt, interfaceObject = interface)
+    controller.enableController()
 
     start=time.time()
-    while(time.time() - start < 20):
-        interface.turnKilnOn(True)
-        interface.readLineFromArduino()
-        print('Kiln Temperature: {}'.format(interface.kilnTemperature))
 
-    interface.dumpData('test.csv')
+    controller.updateSetpoint(460)
+    time.sleep(200)
+    controller.dumpData('test.csv')
 
+    controller.shutdownController()
     interface.disconectSerial()
-
-
-
-
-
-
+    exit()
